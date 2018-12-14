8c05707ce5e5beca93e32a12cfa662b44d8b0b73
Summer <summer@learnku.com>
2018-12-14 11:44:06 +0000

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -8,7 +8,12 @@ class StaticPagesController extends Controller
 {
     public function home()
     {
-        return view('static_pages/home');
+        $feed_items = [];
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(30);
+        }
+
+        return view('static_pages/home', compact('feed_items'));
     }
 
     public function help()
