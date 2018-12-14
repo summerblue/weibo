28090906d66fd90be25d83d3bb5f9512b6a7411c
Summer <summer@learnku.com>
2018-12-14 11:42:55 +0000

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+    protected $fillable = ['content'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
