f997bd66e2309854e74f0900e21a05b6d111e08a
Summer <summer@learnku.com>
2018-12-14 09:39:55 +0000

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,4 +27,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
