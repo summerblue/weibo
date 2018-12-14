d53315b61f46a0d7a7f55c2aea38794445e5d061
Summer <summer@learnku.com>
2018-12-14 09:27:06 +0000

    命名路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container ">
-    <a class="navbar-brand" href="/">Weibo App</a>
+    <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
     <ul class="navbar-nav justify-content-end">
-      <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+      <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
       <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
     </ul>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
