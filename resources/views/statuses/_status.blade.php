9d01e2efbdab93103607bc171bbc525b473d3623
Summer <summer@learnku.com>
2018-12-14 11:40:42 +0000

    用户微博列表

---
+++ resources/views/statuses/_status.blade.php
@@ -0,0 +1,9 @@
+<li class="media mt-4 mb-4">
+  <a href="{{ route('users.show', $user->id )}}">
+    <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="mr-3 gravatar"/>
+  </a>
+  <div class="media-body">
+    <h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small></h5>
+    {{ $status->content }}
+  </div>
+</li>
