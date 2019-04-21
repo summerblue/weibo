8da07925a6b7fd1d35f648e16d2b17d6650c2e6a
Summer <summer@learnku.com>
2019-04-21 09:35:22 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,5 @@
 Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help');
 Route::get('/about', 'StaticPagesController@about');
+
+Route::get('signup', 'UsersController@create')->name('signup');
