1f7a97b834d6160b77af39b4590c648410969517
Summer <summer@learnku.com>
2019-04-22 02:52:53 +0000

    关注和粉丝列表页面

--- resources/views/shared/_stats.blade.php
+++ resources/views/shared/_stats.blade.php
@@ -1,16 +1,16 @@
-<a href="#">
+<a href="{{ route('users.followings', $user->id) }}">
   <strong id="following" class="stat">
     {{ count($user->followings) }}
   </strong>
   关注
 </a>
-<a href="#">
+<a href="{{ route('users.followers', $user->id) }}">
   <strong id="followers" class="stat">
     {{ count($user->followers) }}
   </strong>
   粉丝
 </a>
-<a href="#">
+<a href="{{ route('users.show', $user->id) }}">
   <strong id="statuses" class="stat">
     {{ $user->statuses()->count() }}
   </strong>
