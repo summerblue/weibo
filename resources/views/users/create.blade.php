be7483bc91cae2f8c3c43d8f355e39edae31f9f3
Summer <summer@learnku.com>
2019-09-18 11:11:59 +0000

    增加 CSRF 验证

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,27 +9,29 @@
     </div>
     <div class="card-body">
       <form method="POST" action="{{ route('users.store') }}">
-          <div class="form-group">
-            <label for="name">名称：</label>
-            <input type="text" name="name" class="form-control" value="{{ old('name') }}">
-          </div>
+        {{ csrf_field() }}
 
-          <div class="form-group">
-            <label for="email">邮箱：</label>
-            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
-          </div>
+        <div class="form-group">
+          <label for="name">名称：</label>
+          <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+        </div>
 
-          <div class="form-group">
-            <label for="password">密码：</label>
-            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
-          </div>
+        <div class="form-group">
+          <label for="email">邮箱：</label>
+          <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+        </div>
 
-          <div class="form-group">
-            <label for="password_confirmation">确认密码：</label>
-            <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
-          </div>
+        <div class="form-group">
+          <label for="password">密码：</label>
+          <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+        </div>
 
-          <button type="submit" class="btn btn-primary">注册</button>
+        <div class="form-group">
+          <label for="password_confirmation">确认密码：</label>
+          <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+        </div>
+
+        <button type="submit" class="btn btn-primary">注册</button>
       </form>
     </div>
   </div>
