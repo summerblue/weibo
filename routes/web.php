116c712eeeb71ad572ac50f81f39b9834b23cc51
Summer <summer@learnku.com>
2019-09-19 03:00:52 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
