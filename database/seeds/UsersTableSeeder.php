76673723f20535cf95892545c7dfa0170fea3cd5
Summer <summer@learnku.com>
2019-09-19 03:42:17 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,6 +13,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Summer';
         $user->email = 'summer@example.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -3,4 +3,11 @@
   <a href="{{ route('users.show', $user) }}">
     {{ $user->name }}
   </a>
+  @can('destroy', $user)
+    <form action="{{ route('users.destroy', $user->id) }}" method="post" class="float-right">
+      {{ csrf_field() }}
+      {{ method_field('DELETE') }}
+      <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
+    </form>
+  @endcan
 </div>
