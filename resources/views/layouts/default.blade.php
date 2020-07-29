7b957b355b248b55aa22926795cddb21627e6ef9
Summer <summer@learnku.com>
2020-07-29 02:17:16 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
   </body>
 </html>
