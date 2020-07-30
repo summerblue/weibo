311bd890f468ef432b9e2a03bf6bfc19871ace8b
Summer <summer@learnku.com>
2020-07-30 09:57:57 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,10 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 }
