759136e99cf46a5956b2e936580828086757cc51
Summer <summer@learnku.com>
2020-07-30 03:26:41 +0000

    访问策略

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,6 +19,11 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
+            session()->flash('info', '您已登录，无需再次操作。');
+            return redirect('/');
+        }
+
+        if (Auth::guard($guard)->check()) {
             return redirect(RouteServiceProvider::HOME);
         }
 
