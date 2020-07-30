c368d5967b1bd69c6f6711d44a74b4b5f516edb9
Summer <summer@learnku.com>
2020-07-30 11:17:11 +0000

    社交统计信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,6 +7,10 @@
     <section class="user_info">
       @include('shared._user_info', ['user' => $user])
     </section>
+    <section class="stats mt-2">
+      @include('shared._stats', ['user' => $user])
+    </section>
+    <hr>
     <section class="status">
       @if ($statuses->count() > 0)
         <ul class="list-unstyled">
