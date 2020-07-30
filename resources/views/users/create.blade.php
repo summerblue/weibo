b798f62276e5595dd1619a2963c6500e2063e020
Summer <summer@learnku.com>
2020-07-30 01:03:30 +0000

    中文的错误提示信息

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,6 +8,9 @@
       <h5>注册</h5>
     </div>
     <div class="card-body">
+
+      @include('shared._errors')
+
       <form method="POST" action="{{ route('users.store') }}">
         {{ csrf_field() }}
 
