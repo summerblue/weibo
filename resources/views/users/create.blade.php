1f9f6c8b8ed8b3d89e647e5f1b63dd4e378c544d
Summer <summer@learnku.com>
2020-12-16 03:29:12 +0000

    增加 CSRF 验证

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,6 +9,8 @@
     </div>
     <div class="card-body">
       <form method="POST" action="{{ route('users.store') }}">
+        {{ csrf_field() }}
+
         <div class="form-group">
           <label for="name">名称：</label>
           <input type="text" name="name" class="form-control" value="{{ old('name') }}">
