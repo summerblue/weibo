e752bc210c1a3db3cce68a7f7974505fc8762498
Summer <summer@learnku.com>
2020-12-16 12:01:21 +0000

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name(
 Route::post('password/reset',  'PasswordController@reset')->name('password.update');
 
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
