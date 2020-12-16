f406a8015ff4af8afddc119d2ab0a5c5e33ed1a4
Summer <summer@learnku.com>
2020-12-16 11:59:23 +0000

    社交统计信息

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UsersTableSeeder::class);
         $this->call(StatusesTableSeeder::class);
+        $this->call(FollowersTableSeeder::class);
 
         Model::reguard();
     }
