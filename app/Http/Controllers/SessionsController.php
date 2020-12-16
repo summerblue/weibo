eb40dcc435d729df85756f769247322544d5ef1d
Summer <summer@learnku.com>
2020-12-16 04:57:31 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,4 +27,11 @@ class SessionsController extends Controller
            return redirect()->back()->withInput();
        }
     }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
 }
