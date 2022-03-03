46cdf6e76fa9ae18faffd31ad744ca1661313885
Summer <summer@learnku.com>
2022-03-03 17:25:30 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -10,7 +10,7 @@
       一切，将从这里开始。
     </p>
     <p>
-      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
     </p>
   </div>
 @stop
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('signup', 'UsersController@create')->name('signup');
