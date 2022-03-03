001c8be13f9c777b729ecc9bd713a9fe03c7a851
Summer <summer@learnku.com>
2022-03-03 17:05:59 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="https://learnku.com/laravel/courses" target=_blank class="text-decoration-none">
+    刻意练习，每日精进
+  </a>
+
+  <div class="float-end">
+    <a href="/about" class="text-decoration-none">关于</a>
+  </div>
+</footer>
