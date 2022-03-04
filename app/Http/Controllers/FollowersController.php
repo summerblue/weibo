23a6315e3d9fae1b710ab86f5eed58f6bd1e27bd
Summer <summer@learnku.com>
2022-03-04 22:28:11 +0800

    关注按钮

---
+++ app/Http/Controllers/FollowersController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class FollowersController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(User $user)
+    {
+        $this->authorize('follow', $user);
+
+        if ( ! Auth::user()->isFollowing($user->id)) {
+            Auth::user()->follow($user->id);
+        }
+
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('follow', $user);
+
+        if (Auth::user()->isFollowing($user->id)) {
+            Auth::user()->unfollow($user->id);
+        }
+
+        return redirect()->route('users.show', $user->id);
+    }
+}
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,6 +7,11 @@
     <section class="user_info">
       @include('shared._user_info', ['user' => $user])
     </section>
+
+    @if (Auth::check())
+      @include('users._follow_form')
+    @endif
+
     <section class="stats mt-2">
       @include('shared._stats', ['user' => $user])
     </section>
