228c993f30999d6c0b2e6987750bda84a5d4d05d
Summer <summer@learnku.com>
2022-03-04 22:30:24 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,8 +65,11 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+                              ->with('user')
+                              ->orderBy('created_at', 'desc');
     }
 
     public function followers()
