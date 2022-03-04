8e2234606d17704f02a106e56c366670a421be59
Summer <summer@learnku.com>
2022-03-04 22:25:26 +0800

    关注和粉丝列表页面

---
+++ resources/views/users/show_follow.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.default')
+@section('title', $title)
+
+@section('content')
+<div class="offset-md-2 col-md-8">
+  <h2 class="mb-4 text-center">{{ $title }}</h2>
+
+  <div class="list-group list-group-flush">
+    @foreach ($users as $user)
+      <div class="list-group-item">
+        <img class="me-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}" width=32>
+        <a class="text-decoration-none" href="{{ route('users.show', $user) }}">
+          {{ $user->name }}
+        </a>
+      </div>
+
+    @endforeach
+  </div>
+
+  <div class="mt-3">
+    {!! $users->render() !!}
+  </div>
+</div>
+@stop
