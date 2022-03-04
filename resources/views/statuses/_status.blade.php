5be4a78fbff59e78384e6181bdcedaca89ea0077
Summer <summer@learnku.com>
2022-03-04 22:05:31 +0800

    删除微博

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -1,9 +1,18 @@
 <li class="d-flex mt-4 mb-4">
-  <a class="flex-shrink-0" href="{{ route('users.show', $user->id )}}">
-    <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="me-1 gravatar"/>
+  <a class="flex-shrink-0" href="{{ route('users.show', $user->id) }}">
+    <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="me-1 gravatar" />
   </a>
   <div class="flex-grow-1 ms-3">
     <h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small></h5>
     {{ $status->content }}
   </div>
+
+  @can('destroy', $status)
+    <form action="{{ route('statuses.destroy', $status->id) }}" method="POST" onsubmit="return confirm('您确定要删除本条微博吗？');">
+      {{ csrf_field() }}
+      {{ method_field('DELETE') }}
+      <button type="submit" class="btn btn-sm btn-danger status-delete-btn">删除</button>
+    </form>
+  @endcan
+
 </li>
